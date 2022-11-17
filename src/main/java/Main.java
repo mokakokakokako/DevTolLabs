55dcae76347ad15f8830bd50b88652b22a3afbd8
mokakokako <yota24yota@gmail.com>
2022-11-17 18:09:54 +0300

    Second Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,18 @@
 public class Main
 {
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
 
     }
-    public int add(int x, int y)
-    {
+    public int add(int x, int y) {
         return x + y;
+    }
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+    public int substract(int x, int y) {
+        return x - y;
+    }
+    public int divide(int x, int y) {
+        return x / y;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -7,6 +7,9 @@ public class MainTest
     void additional()
     {
         assertEquals(2, main.add(1 ,1));
+        assertEquals(4, main.multiply(2 ,2));
+        assertEquals(2, main.substract(2 ,0));
+        assertEquals(1, main.divide(1 ,1));
     }
 }
 
