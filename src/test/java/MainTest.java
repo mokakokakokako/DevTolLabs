645257941c9d7acb7ae368fe05b83d736308bbcf
mokakokako <yota24yota@gmail.com>
2022-11-17 18:29:36 +0300

    Last Commit

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -4,11 +4,23 @@ public class MainTest
 {
     Main main = new Main();
     @Test
-    void additional()
+    void addTest()
     {
         assertEquals(2, main.add(1 ,1));
+    }
+    @Test
+    void mulTest()
+    {
         assertEquals(4, main.multiply(2 ,2));
+    }
+    @Test
+    void subTest()
+    {
         assertEquals(2, main.substract(2 ,0));
+    }
+    @Test
+    void divTest()
+    {
         assertEquals(1, main.divide(1 ,1));
     }
 }
